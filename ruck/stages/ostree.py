ae4950c37692adff0869b673fcbae743604a6bc5
Charles Short <charles.short@windriver.com>
2024-03-05 18:21:54 +0000

    debos plugin support
    Signed-off-by: Charles Short <charles.short@windriver.com>

--- ruck/stages/ostree.py
+++ ruck/stages/ostree.py
@@ -4,14 +4,13 @@ Copyright (c) 2024 Wind River Systems, Inc.
 SPDX-License-Identifier: Apache-2.0
 
 """
+import hashlib
 import logging
 import os
 import pathlib
 import shutil
 
 from ruck.archive import unpack
-from ruck.mount import mount
-from ruck.mount import umount
 from ruck.ostree import Ostree
 from ruck.stages.base import Base
 from ruck import utils
@@ -43,10 +42,10 @@ class OstreeInitPlugin(OstreeBase):
 
 class OstreePrepPlugin(OstreeBase):
     def run(self):
-        self.logging.info("Creating ostree repository.")
+        self.logging.info("Creating ostree branch.")
 
-        repo = pathlib.Path(self.options.get("repo"))
-        branch = self.options.get("branch")
+        self.repo = pathlib.Path(self.options.get("repo"))
+        self.branch = self.options.get("branch")
         target = self.workspace.joinpath(self.options.get("target"))
 
         self.rootfs = self.workspace.joinpath("rootfs")
@@ -55,72 +54,99 @@ class OstreePrepPlugin(OstreeBase):
         self.rootfs.mkdir(parents=True, exist_ok=True)
         unpack(target, self.rootfs)
 
-        self.logging.info("Moving /etc to /usr/etc.")
-        os.rename(
-            self.rootfs.joinpath("etc"),
-            self.rootfs.joinpath("usr/etc"))
-        self.logging.info("Moving /var/lib/dpkg to /usr/share/dpkg/database")
-        os.rename(
-            self.rootfs.joinpath("var/lib/dpkg"),
-            self.rootfs.joinpath("usr/share/dpkg/database"))
+        self.create_ostree()
 
-        self.logging.info("Clearing /var")
-        shutil.rmtree(self.rootfs.joinpath("var"))
-        self.rootfs.joinpath("var").mkdir(parents=True, exist_ok=True)
-
-        self.logging.info("Installing kernel")
-        for d in self.rootfs.glob("boot/vmlinuz-*"):
-            kver = d.name.removeprefix("vmlinuz-")
-        shutil.copy(
-            self.rootfs.joinpath(f"boot/vmlinuz-{kver}"),
-            self.rootfs.joinpath(f"usr/lib/modules/{kver}/vmlinuz")
-        )
-        self.logging.info("Installing ramdisk")
-        shutil.copy(
-            self.rootfs.joinpath(f"boot/initrd.img-{kver}"),
-            self.rootfs.joinpath(f"usr/lib/modules/{kver}/initrd.img"))
-        
+    def create_ostree(self):
+        self.setup_boot(self.rootfs.joinpath("boot"),
+                        self.rootfs.joinpath("usr/lib/ostree-boot"))
+        self.convert_to_ostree()
         self.logging.info("Commiting to ostree")
         self.ostree.ostree_commit(
             self.rootfs,
-            branch=branch,
-            repo=repo,
+            branch=self.branch,
+            repo=self.repo,
             subject="Initial commit")
+    
+    def convert_to_ostree(self):
+        CRUFT = ["boot/initrd.img", "boot/vmlinuz",
+                 "initrd.img", "initrd.img.old",
+                 "vmlinuz", "vmlinuz.old"]
+        assert self.rootfs is not None and self.rootfs != ""
 
-class OstreeDeployPlugin(OstreeBase):
-    def run(self):
-        self.logging.info("Deploying ostree branch.")
+        # Remove unecessary files
+        self.logging.info("Removing unnecessary files.")
+        for c in CRUFT:
+            try:
+                os.remove(self.rootfs.joinpath(c))
+            except OSError:
+                pass
 
-        repo = pathlib.Path(self.options.get("repo"))
-        branch = self.options.get("branch")
-        image = self.workspace.joinpath(self.options.get("image"))
+        # Setup and split out etc
+        self.logging.info("Moving /etc to /usr/etc.")
+        shutil.move(self.rootfs.joinpath("etc"), self.rootfs.joinpath("usr"))
 
-        self.logging.info(f"Mounting {image}.")
-        self.rootfs = self.workspace.joinpath("rootfs")
-        if self.rootfs.exists():
-            shutil.rmtree(self.rootfs)
+        self.logging.info("Setting up /ostree and /sysroot.")
+        try:
+            self.rootfs.joinpath("ostree").mkdir(parents=True, exist_ok=True)
+            self.rootfs.joinpath("sysroot").mkdir(parents=True, exist_ok=True)
+        except OSError:
+            pass
 
-        mount(image, self.rootfs) 
+        self.logging.info("Setting up symlinks.")
+        TOPLEVEL_LINKS = {
+            "media": "run/media",
+            "mnt": "var/mnt",
+            "opt": "var/opt",
+            "ostree": "sysroot/ostree",
+            "root": "var/roothome",
+            "srv": "var/srv",
+            "usr/local": "../var/usrlocal",
+        }
+        fd = os.open(self.rootfs, os.O_DIRECTORY)
+        for l, t in TOPLEVEL_LINKS.items():
+            shutil.rmtree(self.rootfs.joinpath(l))
+            os.symlink(t, l, dir_fd=fd)
 
-        sysroot = self.rootfs.joinpath("sysroot")
-        ostree_repo = self.rootfs.joinpath("sysroot/ostree/repo")
-        ostree_repo.mkdir(parents=True, exist_ok=True)
-        utils.run_command(
-            ["ostree", "init", "--repo", ostree_repo, "--mode=bare"]
-        )
-        utils.run_command(
-            ["ostree", "config", "--repo", ostree_repo, "--group", "sysroot",
-             "set", "bootloder", "none"])
-        utils.run_command(
-            ["ostree", "pull-local", "--repo", ostree_repo, repo, branch])
+    def setup_boot(self, bootdir, targetdir):
+        """Setup up the ostree bootdir"""
+        vmlinuz = None
+        initrd = None
+        dtbs = None
+        version = None
 
-        utils.run_command(
-            ["ostree", "admin", "init-fs", sysroot])
-        utils.run_command(
-            ["ostree", "admin", "os-init", "--sysroot", sysroot, "debian"])
-        utils.run_command(
-            ["ostree", "admin", "deploy", "--os", "debian",
-             "--sysroot", sysroot, branch,
-             '--karg="rw"', '--karg="console=ttyS0"'])
-        umount(self.rootfs)
+        try:
+            os.mkdir(targetdir)
+        except OSError:
+            pass
 
+        for item in os.listdir(bootdir):
+            if item.startswith("vmlinuz"):
+                assert vmlinuz is None
+                vmlinuz = item
+                _, version = item.split("-", 1)
+            elif item.startswith("initrd.img") or item.startswith("initramfs"):
+                assert initrd is None
+                initrd = item
+            elif item.startswith("dtbs"):
+                assert dtbs is None
+                dtbs = os.path.join(bootdir, item)
+            else:
+                # Move all other artifacts as is
+                shutil.move(os.path.join(bootdir, item), targetdir)
+        assert vmlinuz is not None
+
+        m = hashlib.sha256()
+        m.update(open(os.path.join(bootdir, vmlinuz), mode="rb").read())
+        if initrd is not None:
+            m.update(open(os.path.join(bootdir, initrd), "rb").read())
+
+        csum = m.hexdigest()
+
+        os.rename(os.path.join(bootdir, vmlinuz),
+                  os.path.join(targetdir, vmlinuz + "-" + csum))
+
+        if initrd is not None:
+            os.rename(os.path.join(bootdir, initrd),
+                      os.path.join(targetdir,
+                                   initrd.replace(
+                                       "initrd.img", "initramfs") + "-" + csum))
