5c5cea04a2ae9eccc513dc595573b17265023801
Charles Short <charles.short@windriver.com>
2024-03-26 15:53:40 +0000

    rename image stage to parted
    Signed-off-by: Charles Short <charles.short@windriver.com>

--- ruck/stages/parted.py
+++ ruck/stages/parted.py
@@ -14,64 +14,71 @@ from ruck.stages.base import Base
 from ruck import utils
 
 
-class ImagePlugin(Base):
+class PartedPlugin(Base):
     def __init__(self, state, config, workspace):
         self.state = state
         self.config = config
         self.workspace = workspace
 
         self.logging = logging.getLogger(__name__)
-        self.options = self.config.get("options")
-        self.images = self.options.get("image")
-        self.partitions = self.options.get("partitions")
-        self.filesystems = self.options.get("filesystems")
 
         self.image = None
 
-    def run(self):
-        self.create_image()
-        self.create_label()
-        self.create_partitions()
-        self.create_filesystems()
+    def preflight_check(self):
+        self.images = self.config.options.image
+        self.partitions = self.config.options.partitions
+        self.filesystems = self.config.options.filesystems
 
-    def create_image(self):
-        self.image = self.workspace.joinpath(self.images.get("name"))
-        size = self.images.get("size")
+
+    def run(self):
+        self._create_image()
+        self._create_label()
+        self._create_partitions()
+        self._create_filesystems()
+
+    def post_install(self):
+        pass
+
+    def _create_image(self):
+        """Create a raw disk image."""
+        self.image = self.workspace.joinpath(self.images.name)
+        size = self.images.size
 
         if self.image.exists():
+            self.logging.info(f"Found previous image, removing {self.image}")
             os.unlink(self.image)
 
         self.logging.info(f"Creating {self.image}")
         utils.run_command(["truncate", "-s", size, self.image])
 
-    def create_label(self):
-        label = self.images.get("label")
+    def _create_label(self):
+        """Create a GPT label for the disk."""
+        label = self.images.label
         if self.image.exists():
             # TODO(chuck): add msdos support
             if label not in ["gpt"]:
                 self.logging.error(f"{label} is not a valid type.")
+
+            self.logging.info(f"Creating GPT label for {self.image}.")
             utils.run_command(
                 ["parted", self.image, "--script", "mklabel", label])
 
-    def create_partitions(self):
+    def _create_partitions(self):
         """Use parted to create the partitions."""
         for index, part in enumerate(self.partitions, start=1):
+            self.logging.info(f"Creating parttion for {part.name}.")
             (out, err) = utils.run_command(
-                ["parted", "-s", self.image, "--", "mkpart", part.get("name"),
-                 part.get("start"), part.get("end")])
-            flags = part.get("flags")
-            if flags:
-                for flag in flags:
-                    cmd = f"parted -s {self.image} -- set {index} {flag} on"
-                    utils.run_command(shlex.split(cmd))
-            part_type = part.get("type")
-            if part_type:
-                cmd = f"sfdisk --part-type {self.image} {index} {part_type}"
+                ["parted", "-s", self.image, "--", "mkpart", part.name,
+                 part.start, part.end])
+
+            self.logging.info(f"Setting partition type(s) for {part.name}.")
+            for flag in part.flags:
+                cmd = f"parted -s {self.image} -- set {index} {flag} on"
                 utils.run_command(shlex.split(cmd))
 
-    def create_filesystems(self):
+    def _create_filesystems(self):
         """Setup the image for the filesystems to be formatted."""
-        self.logging.info("Setting up loopback device.")
+        self.logging.info(f"Setting up loopback device for {self.image}.")
         try:
             self.logging.info(f"Creating device map for {self.image}")
             loop = self.losetup()
@@ -79,15 +86,15 @@ class ImagePlugin(Base):
             for index, part in enumerate(self.filesystems, start=1):
                 fs = f"/dev/{os.path.basename(loop)}p{index}"
                 if os.path.exists(fs):
-                    self.mkfs(fs,
-                              part.get("fs"),
-                              part.get("label"),
-                              part.get("name"))
+                    self._mkfs(fs,
+                              part.fs,
+                              part.label,
+                              part.name)
 
         finally:
             subprocess.run(["losetup", "-d", loop], check=True)
 
-    def mkfs(self, fs, fs_type, label, name):
+    def _mkfs(self, fs, fs_type, label, name):
         """Formatting the filesystem."""
         self.logging.info(f"Formatting filesystems for {name}.")
 
